6266de66fe47ae462bfe490bd9cf1c1ea84dff6e
nithramir <batiste.andre53@hotmail.fr>
2017-07-19 01:34:32 +0200

    push

--- database.go
+++ database.go
@@ -4,40 +4,87 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strconv"
 )
+
+type bouton struct {
+	ID    int
+	Titre string
+}
+type room_type struct {
+	ID      int
+	Xav_doc string
+	Titre   string
+	Good    int
+	Bad     int
+	Bouton1 bouton
+	Bouton2 bouton
+	Bouton3 bouton
+	Bouton4 bouton
+}
 
 func init_database(db_user string, db_passw string) *sql.DB {
 	db, err := sql.Open("mysql", db_user+":"+db_passw+"@tcp(127.0.0.1:3306)/")
 	handle_err(err)
-	db.Query("DROP TABLE " + table_name)
 	_, err = db.Exec("CREATE DATABASE if not exists " + ddb_name)
 	handle_err(err)
 	_, err = db.Exec("USE " + ddb_name)
 	handle_err(err)
 	fmt.Println("Connected to database " + ddb_name)
-	rows, err := db.Query("CREATE TABLE if not exists " + table_name + "(ID int NOT NULL AUTO_INCREMENT, Visite int DEFAULT 0, Good int DEFAULT 0, Bad int DEFAULT 0, xav_doc TEXT(5000), date DATETIME, passwd CHAR(255), salle1 int DEFAULT 0, salle2 int DEFAULT 0, salle3 int DEFAULT 0, salle4 int DEFAULT 0, primary key (ID));")
+	rows, err := db.Query("CREATE TABLE if not exists " + table_name + "(ID int NOT NULL AUTO_INCREMENT, Visite int DEFAULT 0, Good int DEFAULT 0, Bad int DEFAULT 0, Titre CHAR(255), image CHAR(255), Xav_doc TEXT(5000), date DATETIME, passwd CHAR(255), salle1 int DEFAULT 0, salle2 int DEFAULT 0, salle3 int DEFAULT 0, salle4 int DEFAULT 0, primary key (ID));")
 	handle_err(err)
 	fmt.Println("Connected to " + table_name)
 	defer rows.Close()
 	return db
 }
 
-func create_room(db *sql.DB, id_act string, xav_doc string, num_salle string) {
-	request := "INSERT INTO " + table_name + "(date, xav_doc) VALUES(NOW(), '" + xav_doc + "');"
-	_, err := db.Query(request)
+func create_room(db *sql.DB, id_act string, Xav_doc string, num_salle string, Titre string) {
+	request := "INSERT INTO " + table_name + "(date, Xav_doc, Titre) VALUES(NOW(), '" + Xav_doc + "', '" + Titre + "');"
+	print(request)
+	print("\n")
+	req, err := db.Query(request)
 	handle_err(err)
+	req.Close()
+	handle_err(err)
+	request = "UPDATE " + table_name + " SET salle" + num_salle + " = (SELECT ID FROM (SELECT MAX(ID) AS ID from " + table_name + ") AS ID) WHERE ID = 1" + id_act + ";"
+	print(request)
+	req, err = db.Query(request)
+	handle_err(err)
+	defer req.Close()
+	print("test\n")
 }
 
-func get_room(db *sql.DB, id_need string) (string, error) {
-	request := "SELECT xav_doc FROM " + table_name + " WHERE id = '" + id_need + "';"
+func get_room(db *sql.DB, id_need string) (room_type, error) {
+
+	var room room_type
+	request := "SELECT ID, Good, Bad, Titre, Xav_doc, salle1, salle2, salle3, salle4 FROM " + table_name + " WHERE id = '" + id_need + "';"
 	rows, err := db.Query(request)
 	handle_err(err)
-	defer rows.Close()
 	for rows.Next() {
-		var code string
-		err := rows.Scan(&code)
+		err := rows.Scan(&room.ID, &room.Good, &room.Bad, &room.Titre, &room.Xav_doc, &room.Bouton1.ID, &room.Bouton2.ID, &room.Bouton3.ID, &room.Bouton4.ID)
+		rows.Close()
 		handle_err(err)
-		return code, nil
+		room.Bouton1.Titre = get_Titre(db, room.Bouton1.ID)
+		room.Bouton2.Titre = get_Titre(db, room.Bouton2.ID)
+		room.Bouton3.Titre = get_Titre(db, room.Bouton3.ID)
+		room.Bouton4.Titre = get_Titre(db, room.Bouton4.ID)
+		print("\nroom\n")
+		fmt.Println(room)
+		return room, nil
 	}
-	return "", errors.New("id not found")
+	return room, errors.New("id not found")
+}
+
+func get_Titre(db *sql.DB, id int) string {
+	print(strconv.Itoa(id))
+	request := "SELECT Titre from " + table_name + " WHERE id = '" + strconv.Itoa(id) + "';"
+	rows, err := db.Query(request)
+	handle_err(err)
+	var Titre string
+	for rows.Next() {
+		rows.Scan(&Titre)
+		rows.Close()
+	}
+	print(Titre)
+	return Titre
 }
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	_ "bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"image"
+	"image/color"
+	"image/draw"
+	_ "image/jpeg"
 	"io/ioutil"
 	"log"
 	"net/http"
+	_ "os"
 	"reflect"
+	_ "strconv"
 )
 
 const (
@@ -24,39 +32,79 @@ func handle_err(err error) {
 }
 
 func create_salle(w http.ResponseWriter, r *http.Request) {
+	print("create_salle")
 	id_act := r.FormValue("id_act")
 	num_room := r.FormValue("num_room")
 	xav_code := r.FormValue("xav_code")
+	titre := r.FormValue("titre")
+	print("ok\n")
+	r.ParseMultipartForm(32 << 20)
+	img, header, err := r.FormFile("uploadfile")
+	handle_err(err)
+	fmt.Println(header.Filename)
 	fmt.Println(reflect.TypeOf(num_room))
-	if (xav_code == "") || (id_act == "") || (num_room == "") {
+	if (xav_code == "") || (id_act == "") || (num_room == "") || (titre == "") || (img == nil) {
 		print("Erreur les formulaires envoyés")
 	} else {
-		print("formulaires bien rentrés, je les insère magueul")
+		print("formulaires bien rentrés, je les insère magueul\n")
 	}
-	create_room(database, id_act, xav_code, num_room)
+	create_room(database, id_act, xav_code, num_room, titre)
 	fmt.Fprintf(w, "ok")
 }
 
 func get_salle(w http.ResponseWriter, r *http.Request) {
-	id_need := r.FormValue("id")
-	xav_code, err := get_room(database, id_need)
+	print("get_salle")
+	//id_need := r.FormValue("id")
+	var room room_type
+	room, _ = get_room(database, "1")
+	b, err := json.Marshal(room)
+	handle_err(err)
 	if err != nil {
 		fmt.Fprintf(w, "Id error")
 	}
-	fmt.Fprintf(w, xav_code)
+	fmt.Fprintf(w, string(b))
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
 	//	title := r.URL.Path[len("/edit/"):]
 	p, _ := ioutil.ReadFile("edit.html")
+	print(p)
 
 	fmt.Fprintf(w, string(p))
 }
 
+func imgHandler(w http.ResponseWriter, r *http.Request) {
+	m := image.NewRGBA(image.Rect(0, 0, 240, 240))
+	blue := color.RGBA{0, 0, 255, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+	file, err := ioutil.ReadFile("img.png")
+	file = []byte("<a href=\"pageone.php\"><img src=\"https://www.w3schools.com/css/img_fjords.jpg\"  /></a>")
+	handle_err(err)
+	/*buffer := new(bytes.Buffer)
+	var img image.Image = m
+	if err := jpeg.Encode(buffer, img, nil); err != nil {
+		log.Println("unable to encode image.")
+	}*/
+
+	//	w.Header().Set("Content-Type", "image/jpeg")
+	//	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	if _, err := w.Write(file); err != nil {
+		log.Println("unable to write image.")
+	}
+}
+
 func main() {
+
+	type Test struct {
+		Chaine string
+		Entier int
+	}
 	database = init_database("root", "1234")
+	create_room(database, "3", "swagman", "2", "bat-room")
+
 	http.HandleFunc("/get_salle/", get_salle)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/create_salle/", create_salle)
+	http.HandleFunc("/img/", imgHandler)
 	http.ListenAndServe(":8080", nil)
 }
